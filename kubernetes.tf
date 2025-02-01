1655c0da5146629cb7fef458d3d3af398a80c4c6
Joel Arias Cordova <joel_arias@Joels-MacBook-Pro.local>
2025-02-01 14:01:39 -0600

    first commit

---
+++ kubernetes.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+
+resource "aws_instance" "worker_node_1" {
+  ami           = "ami-07d2649d67dbe8900"
+  instance_type = "t1.medium"
+
+  tags = {
+    Name = "kubernetes-worker-node-1"
+  }
+}
+
+resource "aws_instance" "worker_node_2" {
+  ami           = "ami-07d2649d67dbe8900"
+  instance_type = "t1.medium"
+
+  tags = {
+    Name = "kubernetes-worker-node-1"
+  }
+}
+
+resource "aws_instance" "kubernetes_master" {
+  ami           = "ami-07d2649d67dbe8900"
+  instance_type = "t1.medium"
+
+  tags = {
+    Name = "kubernetes-master-node"
+  }
+}
+
+output "kubernetes_worker_node_1_public_ip" {
+  value = aws_instance.worker_node_1.public_ip
+}
+
+output "kubernetes_worker_node_2_public_ip" {
+  value = aws_instance.worker_node_2.public_ip
+}
+
+output "kubernetes_master_public_ip" {
+  value = aws_instance.kubernetes_master.public_ip
+}
